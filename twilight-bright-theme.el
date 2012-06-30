c461673b8962ac3f9e5f0df276d2f2186a2e6ee0
Jim Myhrberg <contact@jimeh.me>
2012-06-30 11:55:41 +0100

    Update header comment, adding author, license, and credit info

--- twilight-bright-theme.el
+++ twilight-bright-theme.el
@@ -1,10 +1,46 @@
-;;; twilight-bright-theme.el --- custom theme for faces
-
+;;; twilight-bright-theme.el --- A Emacs 24 faces port of the TextMate theme
+;;
+;; Copyright (c) 2012 Jim Myhrberg.
+;;
+;; Author: Jim Myhrberg <contact@jimeh.me>
+;; Maintainer: Jim Myhrberg <contact@jimeh.me>
+;; Version: 0.1.0
+;; Keywords: themes
+;; URL: https://github.com/jimeh/twilight-bright-theme.el
+;;
+;; This file is NOT part of GNU Emacs.
+;;
+;;; License:
+;;
+;; Permission is hereby granted, free of charge, to any person obtaining a
+;; copy of this software and associated documentation files (the "Software"),
+;; to deal in the Software without restriction, including without limitation
+;; the rights to use, copy, modify, merge, publish, distribute, sublicense,
+;; and/or sell copies of the Software, and to permit persons to whom the
+;; Software is furnished to do so, subject to the following conditions:
+;;
+;; The above copyright notice and this permission notice shall be included in
+;; all copies or substantial portions of the Software.
+;;
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+;; FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+;; DEALINGS IN THE SOFTWARE.
+;;
+;;; Credit:
+;;
+;; The original TextMate version created by Florian Pichler is available here:
+;; http://einserver.de/goodies
+;;
 ;;; Commentary:
 ;;
-;;; Twilight Bright Theme
+;; This is not a straight up port as some colors have been slightly tweaked
+;; and some were simply invented as the original TextMate version didn't
+;; include any colors that would fit.
 ;;
-
 ;;; Code:
 
 (deftheme twilight-bright
