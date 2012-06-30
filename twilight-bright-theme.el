ed54cd64feca19e42bf1bf60ba3486a17492b9c2
Jim Myhrberg <contact@jimeh.me>
2012-06-30 22:41:28 +0100

    Add autoload code

--- twilight-bright-theme.el
+++ twilight-bright-theme.el
@@ -167,6 +167,11 @@
    )
   )
 
+;;;###autoload
+(when load-file-name
+  (add-to-list 'custom-theme-load-path
+               (file-name-as-directory (file-name-directory load-file-name))))
+
 (provide-theme 'twilight-bright)
 
 ;;; twilight-bright-theme.el ends here
